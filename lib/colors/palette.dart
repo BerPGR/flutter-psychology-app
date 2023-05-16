1741ec3015d7b6b2f244b918af7411d2b2fa24fa
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2023-05-16 17:59:35 -0300

    Added packages, main colors and first screen

---
+++ lib/colors/palette.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const Color dark_purple = Color(0xFF1D1128);
+  static const Color davy_grey = Color(0xFF514F59);
+  static const Color rebecca_purple = Color(0xFF5941A9);
+  static const Color slate_blue = Color(0xFF6D72C3);
+  static const Color pale_purple = Color(0xFFE5D4ED);
+}
